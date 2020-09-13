ecaa5216e6f7579e78fdcf6b225731edcca11969
Ricardo Morin <morinricardo@gmail.com>
2020-09-12 18:16:21 -0700

    Use PT

--- update_date.sh
+++ update_date.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "{\"lastUpdated\": \"`date "+%b %d, %Y"`\"}" > public/lastUpdated.json
+echo "{\"lastUpdated\": \"` TZ=":US/Pacific" date "+%b %d, %Y"`\"}" > public/lastUpdated.json
